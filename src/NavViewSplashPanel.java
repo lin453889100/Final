07219f372940fb4a5efd14038fa7706812abd04b
Yifeng <Yifeng@104.39.88.88>
2016-11-28 14:55:40 -0500

    Yifeng Wu -- add new splash Screen

--- src/NavViewSplashPanel.java
+++ src/NavViewSplashPanel.java
@@ -8,13 +8,27 @@ import javax.swing.*;
 public class NavViewSplashPanel extends JPanel{
     JButton splashButton;
     JLabel optionsLabel;
+     JLabel information1;
+    JLabel information2;
     NavViewSplashPanel()
     {
-            splashButton = new JButton(new ImageIcon("src/images/codeattacks.png"));            
+            splashButton = new JButton(new ImageIcon("C:\\Users\\yifeng\\Desktop\\background.jpg"));            
             add(splashButton);
             
             optionsLabel = new JLabel("No options selected yet.");
             add(optionsLabel);
+            
+            information1 = new JLabel ("");
+           information1.setHorizontalTextPosition(JButton.CENTER);
+            information1.setVerticalTextPosition(JButton.BOTTOM);
+          add(information1);
+          
+          
+          
+          information2 = new JLabel (" .");
+          information2.setHorizontalTextPosition(JButton.CENTER);       
+            information2.setVerticalTextPosition(JButton.CENTER);
+          add(information2);
             
     }    
 }
